93fe3d99c8774121659a1208f2dd662f539d904e
gilzafire <gilza.ab@gmail.com>
2024-03-01 22:42:32 +0100

    before merge

--- Assets/Script/TimeManager.cs
+++ Assets/Script/TimeManager.cs
@@ -8,6 +8,7 @@ public class TimeManager : MonoBehaviour
     public static TimeManager instance;
 
     public GameObject MainGameCanvas;
+    [SerializeField] private GameObject _upgradeCanvas;
     [SerializeField] private TextMeshProUGUI _TimeCountText;
     [SerializeField] private TextMeshProUGUI _TimeCountperSECText;
     [SerializeField] private GameObject _TachyonObj;
@@ -22,9 +23,50 @@ public class TimeManager : MonoBehaviour
 
     public double CurrentTimeCount { get; set; }
 
-    public double CurrentTimeperSecond { get; set; }
+    public double CurrentTimePerSecond { get; set; }
 
     //upgrades
 
     public double TimePerClickUpgrade { get; set; }
+
+    #region UI Updates
+
+    private void UpdateTimeUI()
+    {
+        _TimeCountText.text = CurrentTimeCount.ToString();
+    }
+
+    private void UpdateTimePerSecondUI()
+    {
+        _TimeCountperSECText.text = CurrentTimePerSecond.ToString() + " P/S";
+    }
+    #endregion
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        UpdateTimeUI();
+        UpdateTimePerSecondUI();
+
+        _upgradeCanvas.SetActive(false);
+        MainGameCanvas.SetActive(true);
+    }
+
+    #region On Time Clicked
+        
+    public void OnTimeClicked()
+    {
+        IncreaseTime();
+    }
+
+    public void IncreaseTime()
+    {
+        CurrentTimeCount += 1 + CurrentTimeCount;
+        UpdateTimeUI();
+    }
+    #endregion
+
 }
