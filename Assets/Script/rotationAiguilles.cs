c105690db777526aaf2eff303410b7c897f6ec7c
pablo <pablo.brochard@gmail.com>
2024-03-03 13:13:06 +0100

    Horloge et chrono

--- Assets/Script/rotationAiguilles.cs
+++ Assets/Script/rotationAiguilles.cs
@@ -10,20 +10,37 @@ public class rotationAiguilles : MonoBehaviour
     [SerializeField] private Image minutes;
     [SerializeField] private Image heures;
     private int i=0;
+    private int tic = 0;
+
+    [SerializeField] GameObject audiomanager;
+    private AudioManager audioman;
 
     public void ClickHorloge()
     {
+        audioman = audiomanager.GetComponent<AudioManager>();
+        if (tic == 0)
+        {
+            audioman.PlaySound(11);
+            tic = 1;
+        }
+        else
+        {
+            audioman.PlaySound(12);
+            tic = 0;
+        }
         minutes.rectTransform.Rotate(Vector3.forward, -6);
         heures.rectTransform.Rotate(Vector3.forward, (float)-0.5);
         i++;
         if (i % 60 == 0)
         {
+            audioman.PlaySound(13);
             TimeManager.instance.CurrentTimeCount += TimeManager.instance.CurrentTimePerSecond * 15;
             TimeManager.instance.TimeMultipler *= 1.05;
 
         }
         if (i % 720 == 0)
         {
+            audioman.PlaySound(14);
             TimeManager.instance.CurrentTimePerSecond = TimeManager.instance.CurrentTimePerSecond * 1.2;
             TimeManager.instance.TimeMultipler *= 1.4;
         }
